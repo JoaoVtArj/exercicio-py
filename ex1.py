2f6a5f7789b0562ee4989ceec033d3bb2fd1a26a
Joao <joaovitorpda22@gmail.com>
2022-08-16 13:53:12 -0300

    commit inicial

--- ex1.py
+++ ex1.py
@@ -1,6 +1,6 @@
 # 9 - fazer um programa pra ler 5 valores e em seguida, mostarr a posiçao onde s encotra o maior e menor valor
 
-lista = [60, 20, 30, 40, 55]
+lista = [60, 20, 30, 40, 55, 10]
 print(f' O Menor Valor se encontra na posiçao {lista.index(min(lista))}')
 print('-_-' * 15)
 print(f' O Maior Valor se encontra na posiçao {lista.index(max(lista))}')
